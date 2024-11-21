a56835115f79c53ac270a0e6ea133477b4a0c84e
mrtaln <murat.alan@ymail.com>
2024-11-21 23:09:46 +0300

    hotfix: Fix a critical bug

--- release.config.js
+++ release.config.js
@@ -1,34 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
-  branches: ['master'], // İzlenecek branch'ler
-  plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-        releaseRules: [
-          { type: 'major', release: 'major' }, // Major artırır
-          { type: 'minor', release: 'minor' }, // Minor artırır
-          { type: 'patch', release: 'patch' }, // Patch artırır
-          { type: 'hotfix', release: 'custom' }, // Hotfix özel işlem
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE'],
+    branches: ['master'],
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          preset: 'conventionalcommits',
+          releaseRules: [
+            { type: 'major', release: 'major' },
+            { type: 'minor', release: 'minor' },
+            { type: 'patch', release: 'patch' },
+            { type: 'hotfix', release: 'patch' },
+          ],
+          parserOpts: {
+            noteKeywords: ['BREAKING CHANGE'],
+          },
         },
-      },
+      ],
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]',
+        },
+      ],
     ],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'], // Güncellenen dosyalar
-        message: 'chore(release): v${nextRelease.version} [skip ci]',
-      },
-    ],
-    './custom-hotfix-plugin.js', // Özel hotfix plugin
-  ],
-  tagFormat: 'v${nextRelease.version}', // Versiyon formatı
-};
+    tagFormat: 'v${nextRelease.version}',
+  };
